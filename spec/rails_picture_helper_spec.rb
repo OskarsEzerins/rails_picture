58270604c482b3dd571e88db72349bffd992ea49
Oskars Ezerins <oskarsezerins@users.noreply.github.com>
2021-03-16 01:17:17 +0200

    Update test

--- spec/rails_picture_helper_spec.rb
+++ spec/rails_picture_helper_spec.rb
@@ -9,39 +9,79 @@ RSpec.describe RailsPicture, type: :feature do
     expect(page).to have_css 'picture'
   end
 
-  it 'shows avif image format source' do
-    within 'picture' do
-      expect(page).to have_css "source[type='image/avif']"
+  describe 'jpeg img tag' do
+    it 'shows avif image format source' do
+      within 'picture' do
+        expect(page).to have_css "source[type='image/avif']"
+      end
+    end
+
+    it 'shows webp image format' do
+      within 'picture' do
+        expect(page).to have_css "source[type='image/webp']"
+      end
+    end
+
+    it 'shows jxl image format' do
+      within 'picture' do
+        expect(page).to have_css "source[type='image/jxl']"
+      end
+    end
+
+    it 'shows img tag' do
+      within 'picture' do
+        expect(page).to have_css 'img'
+      end
+    end
+
+    it 'sets img width' do
+      within 'picture' do
+        expect(page).to have_css "img[width='1000']"
+      end
+    end
+
+    it 'creates alt tag from filename' do
+      within 'picture' do
+        expect(page).to have_css "img[alt='Ruby']"
+      end
     end
   end
 
-  it 'shows webp image format' do
-    within 'picture' do
-      expect(page).to have_css "source[type='image/webp']"
+  xdescribe 'png img tag' do
+    it 'shows avif image format source' do
+      within 'picture'[1] do
+        expect(page).to have_css "source[type='image/avif']"
+      end
     end
-  end
 
-  it 'shows jxl image format' do
-    within 'picture' do
-      expect(page).to have_css "source[type='image/jxl']"
+    it 'shows webp image format' do
+      within 'picture'[1] do
+        expect(page).to have_css "source[type='image/webp']"
+      end
     end
-  end
 
-  it 'shows img tag' do
-    within 'picture' do
-      expect(page).to have_css 'img'
+    it 'shows jxl image format' do
+      within 'picture'[1] do
+        expect(page).to have_css "source[type='image/jxl']"
+      end
     end
-  end
 
-  it 'sets img width' do
-    within 'picture' do
-      expect(page).to have_css "img[width='1000']"
+    it 'shows img tag' do
+      within 'picture'[1] do
+        expect(page).to have_css 'img'
+      end
     end
-  end
 
-  it 'creates alt tag from filename' do
-    within 'picture' do
-      expect(page).to have_css "img[alt='Ruby']"
+    it 'sets img width' do
+      within 'picture'[1] do
+        expect(page).to have_css "img[width='1000']"
+      end
+    end
+
+    it 'creates alt tag from filename' do
+      within 'picture'[1] do
+        expect(page).to have_css "img[alt='Ruby']"
+      end
     end
   end
 end
